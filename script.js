81323ab0fadc1e02bffc8cae4b5473ae90fd0c59
Kameshia Ingram <Kameshialatay@gmail.com>
2020-12-16 09:29:59 -0700

    adding ajax call for UV Index with color response

--- script.js
+++ script.js
@@ -2,61 +2,93 @@
 let currentDay = moment().format('L'); 
 let citySearch = [];
 
-
-$("#searchButton").on('click', function(event){
-    event.preventDefault();
-
-    let queryURL = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/forecast?q=" + $('.userInput').val() + "&appid=984c41e22d016a17febb9302c3224c83";
+function call() {
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + $('.userInput').val() + "&appid=984c41e22d016a17febb9302c3224c83";
 
     
+
+    //https://cors-anywhere.herokuapp.com/
 
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
+        let convertedTemp = ((response.list[0].main.temp - 273.15) * (9 / 5) + 32).toFixed(1);
+        let windSpeed = (response.list[0].wind.speed).toFixed(1);
 
-        let cityName = response.city.name;
-        $("#cityDisplay").text(cityName + " " + "(" + currentDay + ")");
-    
-        let currTemp = response.list[0].main.temp; 
-        let convertedTemp = ((currTemp - 273.15) * (9 / 5) + 32).toFixed(1);
+        $("#cityDisplay").text(response.city.name + " " + "(" + currentDay + ")");
+        
         $("#temperatureDisplay").text("Temperature: " + convertedTemp + " \u00B0F");
 
-        let currentHumidity = response.list[0].main.humidity;
-        $("#humidityDisplay").text("Humidity: " + currentHumidity + "%");
+        $("#humidityDisplay").text("Humidity: " + response.list[0].main.humidity + "%");
 
-        let windSpeed = (response.list[0].wind.speed).toFixed(1);
         $("#windSpeedDisplay").text("Wind Speed: " + windSpeed + " MPH");
+
+
+         let uvQueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.city.coord.lat + "&lon=" + response.city.coord.lon + "&appid=984c41e22d016a17febb9302c3224c83"
+
+        $.ajax({
+            url: uvQueryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            let index = response.value
+            console.log(index)
+
+            if(index < 2.9){
+                $('#uvIndexDisplay').text("UV Index: ").append("<span class='favorable'>" + index + '</span');
+            } else if (index > 3.0 && index < 7.9) {
+                $('#uvIndexDisplay').text("UV Index: ").append("<span class='moderate'>" + index + '</span');
+            } else if (index > 8.0){
+                $('#uvIndexDisplay').text("UV Index: ").append("<span class='severe'>" + index + '</span');
+            }
+
 
     })
 
+
+    })
+}
+
+function getItems() {
     if (localStorage.getItem('myCities') !== null) {
-        
-        citySearch.push($('.userInput').val());
-        localStorage.setItem('myCities', JSON.stringify(citySearch));
-       
+        let mySearch = JSON.parse(localStorage.getItem('myCities'));
+
+        for (let i = 0; i < mySearch.length; i++) {
+            let displayDiv = $('<div>');
+            displayDiv.addClass("search-div")
+            displayDiv.text(mySearch[i]);
+            $("#searchHistory").append(displayDiv)
+
+        }
     }
-       
-    getItems()  
+
+}
+
+getItems()
+
+
+$("#searchButton").on('click', function(event){
+    event.preventDefault();
+    
+    call();
+
+    let value = $('.userInput').val()
+
+    if (localStorage.getItem('myCities') !== null) {
+        let existing = JSON.parse(localStorage.getItem('myCities'));
+        existing.push(value)
+        localStorage.setItem('myCities', JSON.stringify(existing))
+        
+    } 
+     else if (localStorage.getItem('myCities') === null){
+        citySearch.push(value);
+        localStorage.setItem('myCities', JSON.stringify(citySearch));
+    }
+
     
 }) 
  
-function getItems(){
-    if (localStorage.getItem('myCities') !== null) {
-    let mySearch = JSON.parse(localStorage.getItem('myCities'));
-   
-    for (let i = 0; i < mySearch.length; i++){
-        let displayDiv = $('<div>');
-        displayDiv.addClass("search-div")
-        displayDiv.text(mySearch[i]);
-        $("#searchHistory").append(displayDiv)
-    } 
-
-    }
-}
-
-//getItems()
-
-
 
